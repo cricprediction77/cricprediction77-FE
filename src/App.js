e6562e0fd1354b8eccdb2b123fd44ea5f95cceb1
cricprediction77 <cricprediction77@gmail.com>
2025-12-31 12:11:18 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import BookingPage from "./pages/BookingPage";
 import ViewPrediction from "./pages/ViewPrediction";
 import Login from "./pages/Login";
-import Admin from "./pages/Admin"
+import Admin from "./pages/Admin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        <Route path="/index.html" element={<Navigate to="/" replace />} />
+
         <Route path="/" element={<Home />} />
         <Route path="/buy-now" element={<BookingPage />} />
         <Route path="/view-prediction" element={<ViewPrediction />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
+
       </Routes>
     </BrowserRouter>
   );
